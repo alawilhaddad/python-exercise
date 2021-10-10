778f54896d7346bba8b5480dba1c7165775f2119
Haddaddegusti <haddad.gusti@gmail.com>
2021-10-10 09:58:06 +0800

    Add: Initialize 05_loops.py

---
+++ 30 Days Python/05_loops.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/30-loops/problem
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(1, 11):
+        print(f'{n} x {i} = {n * i}')
