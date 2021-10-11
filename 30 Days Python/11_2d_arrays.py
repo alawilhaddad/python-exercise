79d7a6afe7364b490d49fb7b11c61fe350eba392
Haddaddegusti <haddad.gusti@gmail.com>
2021-10-11 22:16:48 +0800

    Add: Initialize 11_2d_arrays.py

---
+++ 30 Days Python/11_2d_arrays.py
@@ -0,0 +1,15 @@
+def max_hourglass(arr):
+    max = 0
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            if i == j == 0:
+                max = sum
+            max = sum if max < sum else max
+    return max
+
+if __name__ == "__main__":
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    print(max_hourglass(arr))
