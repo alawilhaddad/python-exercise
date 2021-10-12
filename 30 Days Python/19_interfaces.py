008d8ad58e54a00e7662979c71ed59dd93fba7fa
Haddaddegusti <haddad.gusti@gmail.com>
2021-10-12 14:14:45 +0800

    Add: Initialize 19_interfaces.py

---
+++ 30 Days Python/19_interfaces.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/30-interfaces/problem
+
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, number):
+        divisor = []
+        for i in range(number):
+            if n % (i + 1) == 0:
+                divisor.append(i + 1)
+        return sum(divisor)
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
