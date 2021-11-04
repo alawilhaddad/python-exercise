f481f25cc0ae19c1cfb216b370035a2f8056a82b
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-04 16:59:40 +0800

    create: create 37_same_value.py

---
+++ 100 Challanges/37_same_value.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    dictionary = {}
+    is_equal = None
+    for i in range(int(input('length of first dictionary: '))):
+        key_value = input(f'new key-value (separate with space): ').split()
+        dictionary[key_value[0]] = key_value[1]
+
+    for i, value1 in enumerate(dictionary.values()):
+        for j, value2 in enumerate(dictionary.values()):
+            is_equal = True if value1 == value2 else False
+    print(is_equal)
