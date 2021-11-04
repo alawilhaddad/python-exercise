7ab6c11f5134715a9583b957efef560ab85965d6
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-04 08:59:13 +0800

    create: create 08_replace_char.py

---
+++ 100 Challanges/08_replace_char.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    text = input('input string: ')
+    curr_char = input('character to replace: ')
+    new_char = input('character replacement: ')
+    print(text.replace(curr_char, new_char))
