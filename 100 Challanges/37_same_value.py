470491ee50f6f32b090d4055f93bd6083638e212
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-04 17:05:11 +0800

    edit: add "empty" condition in 37_same_value.py

--- 100 Challanges/37_same_value.py
+++ 100 Challanges/37_same_value.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
         key_value = input(f'new key-value (separate with space): ').split()
         dictionary[key_value[0]] = key_value[1]
 
-    for i, value1 in enumerate(dictionary.values()):
-        for j, value2 in enumerate(dictionary.values()):
-            is_equal = True if value1 == value2 else False
-    print(is_equal)
+    if dictionary != {}:
+        for i, value1 in enumerate(dictionary.values()):
+            for j, value2 in enumerate(dictionary.values()):
+                is_equal = True if value1 == value2 else False
+        print(is_equal)
+    else:
+        print("Empty")
