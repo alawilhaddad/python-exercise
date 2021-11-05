5407ec6d6350b935b02b3d77c83e61eab2b0f345
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-05 17:42:26 +0800

    edit: change 78_palindrome.py to recursive function

--- 100 Challanges/78_palindrome.py
+++ 100 Challanges/78_palindrome.py
@@ -1,6 +1,10 @@
-if __name__ == "__main__":
-    text = input("input text: ")
+def palindrome(string):
     if text.lower()[::-1] == text.lower():
         print("text is palindrome")
     else:
         print("text is not a palindrome")
+
+
+if __name__ == "__main__":
+    text = input("input text: ")
+    palindrome(text)
