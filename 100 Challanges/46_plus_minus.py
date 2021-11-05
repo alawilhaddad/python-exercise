d8a71ca3826d4194261345cb1e66891a4cc5b394
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-05 08:30:11 +0800

    create: create 46_plus_minus.py

---
+++ 100 Challanges/46_plus_minus.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    number = int(input("input number: "))
+    if number > 0:
+        print("positive")
+    elif number < 0:
+        print("negative")
+    else:
+        print("Zero")
