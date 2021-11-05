62a65b42c33fbe65ab043127d9b5b3c7c0023d54
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-05 11:23:16 +0800

    create: create 57_suit.py

---
+++ 100 Challanges/57_suit.py
@@ -0,0 +1,24 @@
+import random
+
+if __name__ == "__main__":
+    selection = ['Rock', 'Paper', 'Scissor']
+    condition = None
+    print("====== Welcome to the game ======")
+    print("Please enter Rock, Paper, or Scissors below:")
+    player = input().title()
+    computer = random.choice(selection)
+    if player == computer:
+        condition = 'tie'
+    elif player == "Rock" and computer == "Paper":
+        condition = "lose"
+    elif player == "Rock" and computer == "Scissors":
+        condition = "win"
+    elif player == "Paper" and computer == "Scissors":
+        condition = "lose"
+    elif player == "Paper" and computer == "Rock":
+        condition = "win"
+    elif player == "Scissors" and computer == "Paper":
+        condition = "win"
+    elif player == "Scissors" and computer == "Rock":
+        condition = "lose"
+    print(f"{condition.title()}! Your opponent choose {computer}")
