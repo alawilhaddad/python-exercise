a7774b8a367004fc819b454b245363437ae35267
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-06 12:43:44 +0800

    create: create < to > 89_char_count.py and its text

---
+++ 100 Challanges/89_char_count.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    file_path = '89_char_count.txt'
+    frequency = {}
+
+    with open(file_path) as file:
+        lines = list(map(lambda x: x.rstrip("\n"), file.readlines()))
+        lines = list(map(lambda x: x.replace(" ", ""), lines))
+    count = 0
+    for sentence in lines:
+        for char in sentence:
+            count += 1
+
+    print(count)
