98186f7ee8d74ae44c905a06b6a182184bb7305a
Haddaddegusti <haddad.gusti@gmail.com>
2021-11-06 10:16:21 +0800

    create: create 84_first_n_lines.py and its text

---
+++ 100 Challanges/84_first_n_lines.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    file_path = '84_first_n_lines.txt'
+    content = []
+    n = int(input("input n: "))
+
+    with open(file_path) as file:
+        lines = file.readlines()
+        lines_num = len(file.readlines())
+
+    if n < lines_num:
+        for i in range(n):
+            print(lines[i].rstrip('\n'))
+
+    else:
+        print("Please enter a valid value. The file has 4 lines. ")
